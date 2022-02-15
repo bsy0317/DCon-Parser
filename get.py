18dd34ea8c4226cd1958649fac1df8945f0e4ba8
Bae Seoyeon <talk@kakao.one>
2022-02-15 16:04:15 +0900

    텔레그램 연동 뼈대 구축

--- get.py
+++ get.py
@@ -4,33 +4,39 @@ import json
 
 class DC_CON:
     def getList(self,package_name):
-        main_dict = dict()
-        data = requests.get(f"https://dccon.dcinside.com/hot/1/title/{package_name}",headers=self.default_headers())
-        data_sec_1 = data.text.split('<li class="div_package "')
-        for i in range(1,len(data_sec_1)):
-            data_sec_2 = data_sec_1[i].split('</li>')[0]
-            package_idx = data_sec_2.split('package_idx="')[1].split('">')[0]
-            dcon_name = data_sec_2.split('<strong class="dcon_name">')[1].split('</strong>')[0]
-            dcon_seller = data_sec_2.split('<span class="dcon_seller">')[1].split('</span>')[0]
-            main_dict[package_idx] = [package_idx,dcon_name,dcon_seller]
-        return main_dict
+        try:
+            main_dict = dict()
+            data = requests.get(f"https://dccon.dcinside.com/hot/1/title/{package_name}",headers=self.default_headers())
+            data_sec_1 = data.text.split('<li class="div_package "')
+            for i in range(1,len(data_sec_1)):
+                data_sec_2 = data_sec_1[i].split('</li>')[0]
+                package_idx = data_sec_2.split('package_idx="')[1].split('">')[0]
+                dcon_name = data_sec_2.split('<strong class="dcon_name">')[1].split('</strong>')[0]
+                dcon_seller = data_sec_2.split('<span class="dcon_seller">')[1].split('</span>')[0]
+                main_dict[package_idx] = [package_idx,dcon_name,dcon_seller]
+            return main_dict
+        except Exception as e:
+            return "ERROR"
     
     def getImageCDN(self,package_idx):
-        main_list = list()
-        headers = {
-        'Referer': 'https://dccon.dcinside.com/',
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.102 Safari/537.36',
-        'Content-Type': 'application/x-www-form-urlencoded',
-        'Origin': 'https://dccon.dcinside.com',
-        'Host': 'dccon.dcinside.com',
-        'X-Requested-With': 'XMLHttpRequest',
-        'Accept':'*/*'
-        }
-        data = requests.post("https://dccon.dcinside.com/index/package_detail",headers=headers,data=f"package_idx={package_idx}").content.decode('unicode-escape')
-        json_data = json.loads(data)['detail']
-        for json_data_in in json_data:
-            main_list.append([json_data_in['ext'],f"https://dcimg5.dcinside.com/dccon.php?no={json_data_in['path']}"])
-        return main_list
+        try:
+            main_list = list()
+            headers = {
+            'Referer': 'https://dccon.dcinside.com/',
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.102 Safari/537.36',
+            'Content-Type': 'application/x-www-form-urlencoded',
+            'Origin': 'https://dccon.dcinside.com',
+            'Host': 'dccon.dcinside.com',
+            'X-Requested-With': 'XMLHttpRequest',
+            'Accept':'*/*'
+            }
+            data = requests.post("https://dccon.dcinside.com/index/package_detail",headers=headers,data=f"package_idx={package_idx}").content.decode('unicode-escape')
+            json_data = json.loads(data)['detail']
+            for json_data_in in json_data:
+                main_list.append([json_data_in['ext'],f"https://dcimg5.dcinside.com/dccon.php?no={json_data_in['path']}"])
+            return main_list
+        except Exception as e:
+            return "ERROR"
     
     def default_headers(self):
         return dict({
--- main.py
+++ main.py
@@ -11,6 +11,7 @@ import shutil
 import stat
 import signal
 from get import DC_CON
+import telegram
 
 from rich import print
 from rich import box
@@ -25,12 +26,52 @@ def main():
     console.log(dccon.getImageCDN("17221"))
     return 0
 
+def telegram_bot_init():
+    #https://python-telegram-bot.readthedocs.io/
+    updater = Updater(token='TOKEN', use_context=True)
+    dispatcher = updater.dispatcher
+        
+    start_handler = CommandHandler('start', telegram_start)
+    dispatcher.add_handler(start_handler)
+    search_handler = CommandHandler('search', telegram_search)
+    dispatcher.add_handler(search_handler)
+    get_handler = CommandHandler('get', telegram_get)
+    dispatcher.add_handler(get_handler)
+    
+    updater.start_polling()
+    console.log(dispatcher.bot.get_me())
+    console.log("Start Complete!")
+    updater.idle()
+    console.log(e)
+    return 0
+
+def telegram_get(update, context):
+    return 0
+
+def telegram_search(update, context):
+    keyword = ' '.join(context.args).upper()
+    if keyword == '':                   #인수가 없다면
+        context.bot.send_message(chat_id=update.effective_chat.id, reply_to_message_id=update.effective_message.message_id, text="/search 키워드") #안내메세지 전송
+        return 0
+    list_data = dccon.getList(keyword)
+    if list_data == "ERROR":
+        context.bot.send_message(chat_id=update.effective_chat.id, reply_to_message_id=update.effective_message.message_id, text="검색된 내용이 없습니다.") #안내메세지 전송
+        return 0
+    context.bot.send_message(chat_id=update.effective_chat.id, reply_to_message_id=update.effective_message.message_id,
+    reply_markup= telegram.InlineKeyboardMarkup(inline_keyboard=[[telegram.InlineKeyboardButton("버튼")]]))    
+    return 0
+    
+def telegram_start(update, context):
+    context.bot.send_message(chat_id=update.effective_chat.id, text="/search 키워드")
+    return 0    
+    
 def sigint_handler(signal, frame):
     sys.exit(0)
 
 if __name__ == "__main__":
     signal.signal(signal.SIGINT, sigint_handler)
     try:
+        telegram_bot_init()
         main()
     except Exception as ex:
         console.log(ex)
